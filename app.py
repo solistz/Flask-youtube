ecfd6de75b7465ebf8d17682c38d4e01d4420f16
solistz <solistn78@gmail.com>
2021-12-15 15:53:44 +0200

    create web - dinamics web of DB

--- app.py
+++ app.py
@@ -36,7 +36,14 @@ def info():
 
 @app.route('/posts')
 def posts():
-    return render_template('posts.html')
+    articles = Article.query.order_by(Article.date.desc()).all()
+    return render_template('posts.html', articles=articles)
+
+
+@app.route('/posts/<int:id>')
+def post_detail(id):
+    article = Article.query.get(id)
+    return render_template('post_detail.html', article=article)
 
 
 @app.route('/create-article', methods = ['POST', 'GET'])
@@ -51,7 +58,7 @@ def createarticle():
         try:
             db.session.add(article)
             db.session.commit()
-            return redirect('/')
+            return redirect('/posts')
         except:
             return 'OUPssss'
     else:
